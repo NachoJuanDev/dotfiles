b06cc939005fd6d2a45db616eb8cf1970f112934
Juan Salas-Morales <jisalas1@uc.cl>
2024-05-11 19:08:23 -0400

    feat: configure tmux

--- zsh/help.sh
+++ zsh/help.sh
@@ -15,3 +15,41 @@ function help {
       awk '{printf "  %-15s : %-15s\n", $2, $3}' FS='#@|#:'
   fi
 }
+
+function help-tmux {
+  echo -e "[*] Tmux cheat sheet\n"
+  
+  echo -e "  * Sessions:"
+  echo -e "    tmux new [-s <name session>]"
+  echo -e "    tmux a [-t <name session>]"
+  echo -e "    tmux kill-session -t <name session>"
+  echo -e "    <prefix>, d : detach"
+  echo -e "    <prefix>, 0-9 : select session"
+  echo -e "    <prefix>, w : list sessions (and windows)"
+  
+  echo -e ""
+
+  echo -e "  * Panes:"
+  echo -e "    <prefix>, % : split vertically"
+  echo -e "    <prefix>, \" : split horizontally"
+  echo -e "    <prefix>, x : kill pane"
+  echo -e "    <prefix>, [left|right|up|down] : change pane"
+  echo -e "    <prefix>, { : move pane to left"
+  echo -e "    <prefix>, } : move pane to right"
+  echo -e "    <prefix>, q : show index and move per index"
+  echo -e "    C-h : select pane L"
+  echo -e "    C-j : select pane D"
+  echo -e "    C-k : select pane U"
+  echo -e "    C-l : select pane R"
+
+  echo -e ""
+
+  echo -e "  * Windows:"
+  echo -e "    <prefix>, c : new window"
+  echo -e "    <prefix>, , : rename window"
+  echo -e "    <prefix>, n : next window"
+  echo -e "    <prefix>, p : previous window"
+  echo -e "    <prefix>, & : close window"
+  echo -e "    <prefix>, w : list windows (and sessions)"
+
+}
