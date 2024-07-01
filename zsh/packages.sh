c0e3a4a44f8f160bf2737ee7f050f8d41ba148c0
Juan Salas-Morales <jisalas1@uc.cl>
2024-07-01 17:35:38 -0400

    refactor: remove unnecesary exports

--- zsh/packages.sh
+++ zsh/packages.sh
@@ -22,8 +22,6 @@ esac
 # Android studio
 export ANDROID_HOME=$HOME/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
-export PATH=$PATH:$ANDROID_HOME/tools
-export PATH=$PATH:$ANDROID_HOME/tools/bin
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
 # Docker
