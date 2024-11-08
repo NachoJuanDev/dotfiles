a59f4494190a5a606b7121abfa5b4624ab7b9d93
NachoJuanDev <jisalas1@uc.cl>
2024-11-08 16:13:44 -0300

    feat: add gh

--- zsh/packages.linux.sh
+++ zsh/packages.linux.sh
@@ -4,8 +4,11 @@
 FNM_PATH="/home/dev/.local/share/fnm"
 if [[ -d "$FNM_PATH" ]]; then
   export PATH="/home/dev/.local/share/fnm:$PATH"
-  eval "`fnm env --use-on-cd`"
+  eval "$(fnm env --use-on-cd)"
 fi
 
 # python - pipx
 export PATH="$PATH:/home/dev/.local/bin"
+
+# gh
+eval "$(gh completion -s zsh)"
